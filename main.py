c733aeb5285fdb6424a0d0a8e6d5a2ffa755748f
studewan <89421563+studewan@users.noreply.github.com>
2021-09-12 20:28:01 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,211 @@
+{
+  "uuid": "A095F919-661C-4B7F-9467-4368B345AFD9",
+  "name": "The Mystery",
+  "creator": "Twine",
+  "creatorVersion": "2.3.14",
+  "schemaName": "Harlowe 3 to JSON",
+  "schemaVersion": "0.0.6",
+  "createdAtMs": 1631371628710,
+  "passages": [
+    {
+      "name": "Starting ",
+      "tags": "",
+      "id": "1",
+      "text": "You are about to go through an experience which would change your mindset forever. But first up... a little about the game! In this migrant trail, your taking up the role of an escaping migrant. You will be faced with a few choices which would lead to how the game carries on. \n\n[[Start -> Start your migrant journey!]]",
+      "links": [
+        {
+          "linkText": "Start",
+          "passageName": "Start your migrant journey!",
+          "original": "[[Start -> Start your migrant journey!]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "You are about to go through an experience which would change your mindset forever. But first up... a little about the game! In this migrant trail, your taking up the role of an escaping migrant. You will be faced with a few choices which would lead to how the game carries on."
+    },
+    {
+      "name": " Start your migrant journey!",
+      "tags": "",
+      "id": "2",
+      "text": "Your city is under attack by a terrorist group. Many people you know have been murdered and you are trying to escape whilst keeping a low profile. You need to leave immediately. What is your profession( each profession leads to a different story)?\n\n[[You are a medical student who works at a make shift clinic -> Medical Student]]\n[[You are currently in between jobs which makes you a little skilled -> Undecided]]",
+      "links": [
+        {
+          "linkText": "You are a medical student who works at a make shift clinic",
+          "passageName": "Medical Student",
+          "original": "[[You are a medical student who works at a make shift clinic -> Medical Student]]"
+        },
+        {
+          "linkText": "You are currently in between jobs which makes you a little skilled",
+          "passageName": "Undecided",
+          "original": "[[You are currently in between jobs which makes you a little skilled -> Undecided]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "Your city is under attack by a terrorist group. Many people you know have been murdered and you are trying to escape whilst keeping a low profile. You need to leave immediately. What is your profession( each profession leads to a different story)?"
+    },
+    {
+      "name": " Medical Student",
+      "tags": "",
+      "id": "3",
+      "text": "As the minutes pass the situation gets more and more intense. There are bombs dropping everywhere and the terrorists are stealing around from everyone's house. You need an escape route to find your way out without getting killed...\n\n[[Pay a smuggler to help you escape through the sea. -> Sea escape]]\n[[You decide to escape through the land -> Land escape]]\n[[Help out the people around you to escape (Since you are a medical student you are given a bonus option!!) -> Saviour]]",
+      "links": [
+        {
+          "linkText": "Pay a smuggler to help you escape through the sea.",
+          "passageName": "Sea escape",
+          "original": "[[Pay a smuggler to help you escape through the sea. -> Sea escape]]"
+        },
+        {
+          "linkText": "You decide to escape through the land",
+          "passageName": "Land escape",
+          "original": "[[You decide to escape through the land -> Land escape]]"
+        },
+        {
+          "linkText": "Help out the people around you to escape (Since you are a medical student you are given a bonus option!!)",
+          "passageName": "Saviour",
+          "original": "[[Help out the people around you to escape (Since you are a medical student you are given a bonus option!!) -> Saviour]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "As the minutes pass the situation gets more and more intense. There are bombs dropping everywhere and the terrorists are stealing around from everyone's house. You need an escape route to find your way out without getting killed..."
+    },
+    {
+      "name": " Undecided",
+      "tags": "",
+      "id": "4",
+      "text": "As the minutes pass the situation gets more and more intense. There are bombs dropping everywhere and the terrorists are stealing around from everyone's house. You need an escape route to find your way out without getting killed...\n\n[[Pay a smuggler to help you escape through the sea. -> Sea escape]]\n[[You decide to escape through the land -> Land escape]]\n[[Look for useful items around you to ensure your journey is good (Since your profession is makeshift you are given a bonus option!!) -> Scavenger]]",
+      "links": [
+        {
+          "linkText": "Pay a smuggler to help you escape through the sea.",
+          "passageName": "Sea escape",
+          "original": "[[Pay a smuggler to help you escape through the sea. -> Sea escape]]"
+        },
+        {
+          "linkText": "You decide to escape through the land",
+          "passageName": "Land escape",
+          "original": "[[You decide to escape through the land -> Land escape]]"
+        },
+        {
+          "linkText": "Look for useful items around you to ensure your journey is good (Since your profession is makeshift you are given a bonus option!!)",
+          "passageName": "Scavenger",
+          "original": "[[Look for useful items around you to ensure your journey is good (Since your profession is makeshift you are given a bonus option!!) -> Scavenger]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "As the minutes pass the situation gets more and more intense. There are bombs dropping everywhere and the terrorists are stealing around from everyone's house. You need an escape route to find your way out without getting killed..."
+    },
+    {
+      "name": " Sea escape",
+      "tags": "",
+      "id": "5",
+      "text": "You have decided to smuggle yourself out of your city. You make your way towards the sea and reach the boat to find out that it looks mostly filled to the brim. After a long wait you finally get to have a seat on the ground as there was not enough seats in the seating area. You make your way through the ocean while facing many challenges. There are storms on the way which makes everyone on the boat really cold. Since the boat had to leave in a hurry there was not enough food available for everyone to eat and the whole boat stunk because of how packed everyone was. \n\nFinally after what feels like forever you were able to make it back to the shores of USA. Since you are not documented you have the following options to make sure you do not get caught...\n\n[[You pay the smuggler even more money to make you fake documents -> Illegal]]\n[[You make your way to the nearest embassy of your country and seek shelter -> Legal]]",
+      "links": [
+        {
+          "linkText": "You pay the smuggler even more money to make you fake documents",
+          "passageName": "Illegal",
+          "original": "[[You pay the smuggler even more money to make you fake documents -> Illegal]]"
+        },
+        {
+          "linkText": "You make your way to the nearest embassy of your country and seek shelter",
+          "passageName": "Legal",
+          "original": "[[You make your way to the nearest embassy of your country and seek shelter -> Legal]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "You have decided to smuggle yourself out of your city. You make your way towards the sea and reach the boat to find out that it looks mostly filled to the brim. After a long wait you finally get to have a seat on the ground as there was not enough seats in the seating area. You make your way through the ocean while facing many challenges. There are storms on the way which makes everyone on the boat really cold. Since the boat had to leave in a hurry there was not enough food available for everyone to eat and the whole boat stunk because of how packed everyone was. \n\nFinally after what feels like forever you were able to make it back to the shores of USA. Since you are not documented you have the following options to make sure you do not get caught..."
+    },
+    {
+      "name": " Land escape",
+      "tags": "",
+      "id": "6",
+      "text": "Travel through land has always been hard for everyone. There is always lack of food, water, shelter, and everyone always ends up getting tired. You were lucky enough to have been able to hitch hike your way through most of the journey. But unfortunately you are really exhausted and have no clue where to go.\n\nFinally after what feels like forever you were able to make it back to the shores of USA. Since you are not documented you have the following options to make sure you do not get caught...\n\n[[You came across a smuggler who would make you fake documents -> Illegal]]\n[[You make your way to the nearest embassy of your country and seek shelter -> Legal]]",
+      "links": [
+        {
+          "linkText": "You came across a smuggler who would make you fake documents",
+          "passageName": "Illegal",
+          "original": "[[You came across a smuggler who would make you fake documents -> Illegal]]"
+        },
+        {
+          "linkText": "You make your way to the nearest embassy of your country and seek shelter",
+          "passageName": "Legal",
+          "original": "[[You make your way to the nearest embassy of your country and seek shelter -> Legal]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "Travel through land has always been hard for everyone. There is always lack of food, water, shelter, and everyone always ends up getting tired. You were lucky enough to have been able to hitch hike your way through most of the journey. But unfortunately you are really exhausted and have no clue where to go.\n\nFinally after what feels like forever you were able to make it back to the shores of USA. Since you are not documented you have the following options to make sure you do not get caught..."
+    },
+    {
+      "name": " Saviour",
+      "tags": "",
+      "id": "7",
+      "text": "You chose the option to make sure the people around you are safe. You were successfully able to make your way to the make shift clinic to get a few supplies to help the injured people around you. Whislt helping everyone out, you were able to save the son of a wealthy man who was making his way to the shores of USA to escape. He offers to help you out and takes you along with his family. \n\n[[Winner!!]]",
+      "links": [
+        {
+          "linkText": "Winner!!",
+          "passageName": "Winner!!",
+          "original": "[[Winner!!]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "You chose the option to make sure the people around you are safe. You were successfully able to make your way to the make shift clinic to get a few supplies to help the injured people around you. Whislt helping everyone out, you were able to save the son of a wealthy man who was making his way to the shores of USA to escape. He offers to help you out and takes you along with his family."
+    },
+    {
+      "name": " Scavenger",
+      "tags": "",
+      "id": "8",
+      "text": "You chose the option to make sure that you and the people around you are safe and stocked with a few necessities. You were successfully able to make your way through the city to get a few supplies to help the injured people around you. Whislt helping everyone out and gathering some items, you were able to save the son of a wealthy man who was making his way to the shores of USA to escape. He offers to help you out and takes you along with his family. \n\n[[Winner!!]]",
+      "links": [
+        {
+          "linkText": "Winner!!",
+          "passageName": "Winner!!",
+          "original": "[[Winner!!]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "You chose the option to make sure that you and the people around you are safe and stocked with a few necessities. You were successfully able to make your way through the city to get a few supplies to help the injured people around you. Whislt helping everyone out and gathering some items, you were able to save the son of a wealthy man who was making his way to the shores of USA to escape. He offers to help you out and takes you along with his family."
+    },
+    {
+      "name": " Illegal",
+      "tags": "",
+      "id": "9",
+      "text": "Unfortunately, the choice that you have chosen has led the authorities to be very suspicious of you and ask around about you. They found out that you are in USA illegally which is why you are being deported back. \n\nWould you like to play another round?\n\n[[Starting ]]",
+      "links": [
+        {
+          "linkText": "Starting",
+          "passageName": "Starting",
+          "original": "[[Starting ]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "Unfortunately, the choice that you have chosen has led the authorities to be very suspicious of you and ask around about you. They found out that you are in USA illegally which is why you are being deported back. \n\nWould you like to play another round?"
+    },
+    {
+      "name": " Legal",
+      "tags": "",
+      "id": "10",
+      "text": "You chose the correct options which has led you to safe gound!!\n\n[[Winner!!]]",
+      "links": [
+        {
+          "linkText": "Winner!!",
+          "passageName": "Winner!!",
+          "original": "[[Winner!!]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "You chose the correct options which has led you to safe gound!!"
+    },
+    {
+      "name": "Winner!!",
+      "tags": "",
+      "id": "11",
+      "text": "Congradulations!! You were sucessfully able to make it and you saved many people on the way!! \n\nWould you like to play again?\n\n[[Starting ]]",
+      "links": [
+        {
+          "linkText": "Starting",
+          "passageName": "Starting",
+          "original": "[[Starting ]]"
+        }
+      ],
+      "hooks": [],
+      "cleanText": "Congradulations!! You were sucessfully able to make it and you saved many people on the way!! \n\nWould you like to play again?"
+    }
+  ]
+}
